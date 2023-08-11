d3f4ef6ce5a1156242e1f1d27b25c7a11ec9580f
ATD <kuenen.jacy@gmail.com>
2023-08-11 19:19:02 +0200

    updated some minor stuff

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-// Imports
 const express = require('express')
 const app = express()
 const port = 5000
 
-// Static Files
 app.use(express.static('public'));
 
-// Set View's
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-// Navigation
 app.get('', (req, res) => {
     res.render('index')
 })
