9e272af673c95c8ec2202a1ca884b4cb0ac42afb
ATD <kuenen.jacy@gmail.com>
2023-08-16 19:02:00 +0200

    updates

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const app = express();
+
+const port = 5000;
 
 app.use(express.static('public'));
 
@@ -8,7 +11,17 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('', (req, res) => {
-    res.render('index')
-})
+    res.render('index');
+});
 
-app.listen(port, () => console.info(`App listening on port ${port}`))
+const options = {
+    key: fs.readFileSync('/SSL/whirlcraft_xyz/private-key.pem'),
+    cert: fs.readFileSync('/SSL/whirlcraft_xyz/whirlcraft_xyz.crt'),
+    ca: fs.readFileSync('/SSL/whirlcraft_xyz/whirlcraft_xyz.ca-bundle')
+};
+
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+    console.info(`App listening on port ${port}`);
+});
